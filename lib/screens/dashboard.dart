aedb12120fc94a817ed2337954924c3c31893952
rafipopz98 <itsrealrafi@gmail.com>
2023-11-26 22:06:15 +0530

    dashboard

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,105 +1,147 @@
 import 'package:flutter/material.dart';
 import 'package:hopin/Assistant/request_assistan.dart';
-import 'package:hopin/Model/directions.dart';
 import 'package:hopin/Model/predicted_places.dart';
-import 'package:hopin/global/global.dart';
 import 'package:hopin/global/map_key.dart';
-import 'package:hopin/infoHandler/app_info.dart';
-import 'package:hopin/widgets/progress_dialog.dart';
-import 'package:provider/provider.dart';
+import 'package:hopin/widgets/place_prediction_tile.dart';
 
-class PlacePredictionTileDesign extends StatefulWidget {
-  // const PlacePredictionTileDesign({super.key});
-  final PredictedPlaces? predictedPlaces;
-
-  PlacePredictionTileDesign({this.predictedPlaces});
+class SearchPlacesScreen extends StatefulWidget {
+  const SearchPlacesScreen({super.key});
 
   @override
-  State<PlacePredictionTileDesign> createState() =>
-      _PlacePredictionTileDesignState();
+  State<SearchPlacesScreen> createState() => _SearchPlacesScreenState();
 }
- 
-class _PlacePredictionTileDesignState extends State<PlacePredictionTileDesign> {
-  getPlaceDirectionDetails(String? placeId, context) async {
-    showDialog( 
-      context: context,
-      builder: (BuildContext context) => ProgressDialog(
-        message: "Setting up DropOFF.PLease wait......",
-      ),
-    );
-    String placeDirectionDetailsUrl =
-        "https://maps.googleapis.com/maps/api/place/deatils/json?place_id=$placeId&key=$mapKey";
-    var responseApi =
-        await RequstAssistant.recieveRequest(placeDirectionDetailsUrl);
-    Navigator.pop(context);
-    if (responseApi == "Error Occured,Failed,No Response") {
-      return;
+
+class _SearchPlacesScreenState extends State<SearchPlacesScreen> {
+  List<PredictedPlaces> placesPredictionList = [];
+
+  findPlaceAutoCompleteSearch(String inputText) async {
+    if (inputText.length > 1) {
+      String urlAutocompleteSearch =
+          "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$inputText&key=$mapKey&components=country:IN";
+
+      var responseAutoCompleteSearch =
+          await RequstAssistant.recieveRequest(urlAutocompleteSearch);
+
+      if (responseAutoCompleteSearch == "Error Occured,Failed,No Response") {
+        return;
+      }
+      if (responseAutoCompleteSearch["status"] == "OK") {
+        var placePredictions = responseAutoCompleteSearch["predictions"];
+
+        var placePredictionsList = (placePredictions as List)
+            .map((jsonData) => PredictedPlaces.fromJson(jsonData))
+            .toList();
+
+        setState(() {
+          placesPredictionList = placesPredictionList;
+        });
+      }
     }
-
-    if (responseApi["status"] == "OK") {
-      Directions directions = Directions();
-      directions.locationName = responseApi["result"]["name"];
-      directions.locationid = placeId;
-      directions.locationLatitude =
-          responseApi["result"]["geometry"]["location"]["lat"];
-      directions.locationLongitude =
-          responseApi["result"]["geometry"]["location"]["lng"];
-      // directions.locationLongitude=responseApi["result"]["name"];
-
-      Provider.of<AppInfo>(context, listen: false)
-          .updateDropOffLocationAddress(directions);
-
-      setState(() {
-        userDropOffAddress = directions.locationName!;
-      });
-      Navigator.pop(context, "ObtainedDropOff");
-    } 
-  } 
+  }
 
   @override
   Widget build(BuildContext context) {
     bool darkTheme =
         MediaQuery.of(context).platformBrightness == Brightness.dark;
-    return ElevatedButton(
-      onPressed: () {
-        getPlaceDirectionDetails(widget.predictedPlaces!.place_id, context);
-      },
-      style: ElevatedButton.styleFrom(
-        primary: darkTheme ? Colors.black : Colors.white,
-      ),
-      child: Row(
-        children: [
-          Icon(
-            Icons.add_location,
-            color: darkTheme ? Colors.amber.shade400 : Colors.blue,
+
+    return GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          backgroundColor: darkTheme ? Colors.black : Colors.white,
+          appBar: AppBar(
+            backgroundColor: darkTheme ? Colors.amber.shade400 : Colors.blue,
+            leading: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Icon(Icons.arrow_back,
+                  color: darkTheme ? Colors.black : Colors.white),
+            ),
+            title: Text(
+              "Search DropOff Location",
+              style: TextStyle(color: darkTheme ? Colors.black : Colors.white),
+            ),
+            elevation: 0.0,
           ),
-          SizedBox(
-            width: 10,
-          ),
-          Expanded(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          body: Column(
             children: [
-              Text(
-                widget.predictedPlaces!.main_text!,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: darkTheme ? Colors.amber.shade400 : Colors.blue,
+              Container(
+                decoration: BoxDecoration(
+                    color: darkTheme ? Colors.amber.shade400 : Colors.blue,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.white54,
+                          blurRadius: 8,
+                          spreadRadius: 0.5,
+                          offset: Offset(0.7, 0.7))
+                    ]),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.adjust_sharp,
+                            color: darkTheme ? Colors.black : Colors.white,
+                          ),
+                          SizedBox(
+                            height: 18.0,
+                          ),
+                          Expanded(
+                              child: Padding(
+                            padding: EdgeInsets.all(8),
+                            child: TextField(
+                              onChanged: (value) {
+                                findPlaceAutoCompleteSearch(value);
+                              },
+                              decoration: InputDecoration(
+                                  hintText: "Search Location here ...",
+                                  fillColor:
+                                      darkTheme ? Colors.black : Colors.white54,
+                                  filled: true,
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.only(
+                                    left: 11,
+                                    top: 8,
+                                    bottom: 8,
+                                  )),
+                            ),
+                          ))
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               ),
-              Text(
-                widget.predictedPlaces!.secondary_text!,
-                overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: darkTheme ? Colors.amber.shade400 : Colors.blue,
-                ),
-              ),
+
+              //display place prediction results
+
+              (placesPredictionList.length > 0)
+                  ? Expanded(
+                      child: ListView.separated(
+                        itemCount: placesPredictionList.length,
+                        physics: ClampingScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return PlacePredictionTileDesign(
+                            predictedPlaces: placesPredictionList[index],
+                          );
+                        },
+                        separatorBuilder: (BuildContext context, int index) {
+                          return Divider(
+                            height: 0,
+                            color:
+                                darkTheme ? Colors.amber.shade400 : Colors.blue,
+                            thickness: 0,
+                          );
+                        },
+                      ),
+                    )
+                  : Container()
             ],
-          ))
-        ],
-      ),
-    );
+          ),
+        ));
   }
 }
